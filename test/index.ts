c7275ccbe86eca31e839bd303a4c82ad6a9e7a9f
Toma <62384384+TomaSajt@users.noreply.github.com>
2021-07-03 22:40:37 +0200

    a

--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,13 @@
-import {Joe, Mama} from '../src/index'
+import { Client } from 'discord.js'
+import dotenv from 'dotenv'
+import { CombinedHandler } from '../src/index'
+dotenv.config()
+const client = new Client()
+client.login(process.env.BOT_TOKEN)
+client.on('ready', () => {
+    console.log('Successfully logged in')
+})
 
-
+client.on('message', (aa) => {
+    console.log('got the message')
+})
